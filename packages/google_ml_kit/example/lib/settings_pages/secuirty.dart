476fd8f04c08c89cb980424806cca643363dff31
jakkappro <jakkappro@gmail.com>
2023-02-19 18:51:13 +0100

    redesign Security page

--- packages/google_ml_kit/example/lib/settings_pages/secuirty.dart
+++ packages/google_ml_kit/example/lib/settings_pages/secuirty.dart
@@ -7,169 +7,143 @@ class SecurityPage extends StatefulWidget {
 }
 
 class _SecurityPageState extends State<SecurityPage> {
-  final _passwordController = TextEditingController();
-  bool _isSwitched = false;
   final _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: Color.fromRGBO(56, 45, 62, 1),
-        appBar: AppBar(
-          title: Text('Security', style: TextStyle(fontSize: 30)),
-          backgroundColor: Color.fromRGBO(56, 45, 62, 1),
-          centerTitle: true,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios),
-            iconSize: 25,
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            color: Colors.white,
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(40, 48, 70, 1),
+              Color.fromRGBO(60, 78, 104, 1)
+            ],
+            begin: Alignment.bottomRight,
+            end: Alignment.topLeft,
           ),
-          elevation: 0,
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
-          child: Column(children: [
-            Container(
-              height: 60,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Color.fromRGBO(66, 58, 76, 1),
-              ),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 25.0),
-                    child: Text(
-                      'Email: ',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: const Text('Security', style: TextStyle(fontSize: 30)),
+            backgroundColor: Colors.transparent,
+            centerTitle: true,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              iconSize: 25,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              color: Colors.white,
+            ),
+            elevation: 0,
+          ),
+          body: Padding(
+            padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+            child: Column(children: [
+              Container(
+                height: 60,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color: Colors.transparent,
+                ),
+                child: Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 25.0),
+                      child: Text(
+                        'Email:        ',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(width: 20),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 20.0),
-                      child: TextField(
-                        onSubmitted: (value) async => await _changeEmail(value),
-                        decoration: InputDecoration(
-                          hintText: _auth.currentUser!.email,
-                          hintStyle: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
+                    const SizedBox(width: 20),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        child: TextField(
+                          onSubmitted: (value) async =>
+                              await _changeEmail(value),
+                          decoration: InputDecoration(
+                            hintText: _auth.currentUser!.email,
+                            hintStyle: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              height: 60,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Color.fromRGBO(66, 58, 76, 1),
-              ),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 25.0),
-                    child: Text(
-                      'Password: ',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 20),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 20.0),
-                      child: TextField(
-                        obscureText: true,
-                        onSubmitted: (value) async => _updatePassword(value),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              height: 60,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Color.fromRGBO(66, 58, 76, 1),
-              ),
-              child: Row(children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 25.0),
-                  child: Text(
-                    '2FA: ',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                  ],
                 ),
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Switch(
-                        value: _isSwitched,
-                        onChanged: (bool value) {
-                          setState(() {
-                            _isSwitched = value;
-                          });
-                        },
-                        activeTrackColor: Color.fromRGBO(133, 65, 201, 1),
-                        activeColor: Colors.white,
-                      ),
-                    ],
-                  ),
-                )
-              ]),
-            ),
-            Expanded(
-              child: Padding(
-                padding:
-                    const EdgeInsets.only(bottom: 10.0, left: 10, right: 10),
-                child: Container(
-                  alignment: Alignment.bottomCenter,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      minimumSize: Size(double.infinity, 40),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                height: 60,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color: Colors.transparent,
+                ),
+                child: Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 25.0),
+                      child: Text(
+                        'Password: ',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                    onPressed: () async {
-                      await _auth.currentUser!.reload();
-                      Navigator.pop(context);
-                    },
-                    child: Text(
-                      'Save',
-                      style: TextStyle(fontSize: 20, color: Colors.black),
+                    const SizedBox(width: 20),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        child: TextField(
+                          obscureText: true,
+                          onSubmitted: (value) async => _updatePassword(value),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 10.0, left: 10, right: 10),
+                  child: Container(
+                    alignment: Alignment.bottomCenter,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        minimumSize: const Size(double.infinity, 40),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      onPressed: () async {
+                        await _auth.currentUser!.reload();
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        'Save',
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ]),
+            ]),
+          ),
         ),
       ),
     );
@@ -180,16 +154,12 @@ class _SecurityPageState extends State<SecurityPage> {
       await _auth.currentUser!.updateEmail(value.trim());
       await _auth.signOut();
       Navigator.of(context).pushReplacementNamed('/login');
-    } catch (e) {
-      print('fuk $e');
-    }
+    } catch (e) {}
   }
 
   Future<void> _updatePassword(String value) async {
     try {
       _auth.currentUser!.updatePassword(value);
-    } catch (e) {
-      print('ehm $e');
-    }
+    } catch (e) {}
   }
 }
